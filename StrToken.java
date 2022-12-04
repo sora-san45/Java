f69c87666373277f06d6c3f497f33ac7419837fa
Mary Ann <maryannjose129@gmail.com>
2022-12-04 22:30:30 +0530

    added string tokenzier file

---
+++ StrToken.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class StrToken {
+    public static void main(String args[]){
+        System.out.println("Enter integers :");
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        StringTokenizer st1 = new StringTokenizer(st," ");
+        int sum=0;
+        while(st1.hasMoreTokens()){
+            int num=Integer.parseInt(st1.nextToken());
+            sum+=num;
+            System.out.print(num+" ");
+        }
+        System.out.print("="+sum);
+    }
+    
+}
